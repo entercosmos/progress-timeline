4dd703713b35802f99d35ef53780b2f924b10cc1
keyflight <olivier@andev.nl>
2019-05-06 07:27:03 +0700

    Small improvements

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,63 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react'
+import { Canvas, Box, Heading, Paragraph } from '@cmds/demo-utils'
+import { injectGlobal } from 'emotion'
+import { render } from 'react-dom'
 
-import Example from '../../src'
+injectGlobal`
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+        margin: 0;
+    }
+`
+
+import ProgressTimeline from '../../src'
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>progress-timeline Demo</h1>
-      <Example/>
-    </div>
+    return <Canvas>
+      <Heading>
+        Progress Timeline
+      </Heading>
+      <Paragraph>
+        Default
+      </Paragraph>
+      <Box>
+        <ProgressTimeline
+          steps={[8, 9, 10, 20, 25, 29, 30, 31]}
+          value={24}
+        />
+      </Box>
+      <Paragraph>
+        Without value
+      </Paragraph>
+      <Box>
+        <ProgressTimeline
+          steps={[8, 9, 10, 20, 25, 29, 30, 31]}
+        />
+      </Box>
+      <Paragraph>
+        Value set on the minimum value
+      </Paragraph>
+      <Box>
+        <ProgressTimeline
+          steps={[8, 9, 10, 20, 25, 29, 30, 31]}
+          value={8}
+        />
+      </Box>
+      <Paragraph>
+        Value set on the maximum value
+      </Paragraph>
+      <Box>
+        <ProgressTimeline
+          steps={[8, 9, 10, 20, 25, 29, 30, 31]}
+          value={31}
+        />
+      </Box>
+    </Canvas>
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,181 @@
-import React, {Component} from 'react'
+import React from 'react'
+import { css, cx } from 'emotion'
+import isNil from 'lodash/isNil'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
+const _parseFloat = input => parseFloat(input.toFixed(2))
+
+const LegendItem = ({ children, offset }) => (
+    <div
+        className={cx(
+            'ProgressTimeline__LegendItem',
+            css`
+                position: absolute;
+                height: 100%;
+                width: 40px;
+                margin-left: -20px;
+                text-align: center;
+                font-size: 13px;
+                color: #787878;
+            `
+        )}
+        style={{
+            left: offset + '%'
+        }}
+    >
+        {children}
     </div>
-  }
+)
+
+const ProgressTimeline = ({ steps, value }) => {
+
+    const data = steps.reduce((result, step, index) => {
+
+        if (index === 0) {
+            return result
+        }
+
+        const min = steps[0]
+        const max = steps[steps.length - 1]
+        const total = max - min
+
+        const prevStep = steps[index - 1]
+
+        const size = step - prevStep
+
+        const offset = prevStep - min
+
+        const offsetPercentage = _parseFloat((offset / total) * 100)
+        const sizePercentage = _parseFloat((size / total) * 100)
+
+        result.items.push({
+            prevStep,
+            step,
+            offset,
+            offsetPercentage,
+            sizePercentage,
+            size
+        })
+
+        if (!isNil(result.value) && result.value >= min && result.value <= max) {
+
+            const indicatorOffset = result.value - min
+
+            result.indicatorOffset = indicatorOffset
+            result.indicatorOffsetPercentage = _parseFloat((indicatorOffset / total) * 100)
+            result.indicatorEnabled = true
+        }
+
+        result.min = min
+        result.max = max
+        result.total = total
+
+        return result
+
+    }, {
+            steps,
+            value,
+            items: [],
+            min: 0,
+            max: 0,
+            total: 0,
+            indicatorOffset: 0,
+            indicatorOffsetPercentage: 0,
+            indicatorEnabled: false
+        })
+
+    const { items, indicatorOffsetPercentage, indicatorEnabled, min } = data
+
+    return (
+        <div
+            className={cx(
+                'ProgressTimeline',
+                css`
+                    width: 100%;
+                    position: relative;
+                `
+            )}
+        >
+            <div
+                className={cx(
+                    'ProgressTimeline__ProgressBar',
+                    css`
+                        position: relative;
+                        width: 100%;
+                        height: 10px;
+                        background-color: rgba(70,85,105,.2);
+                        border-radius: 3px;
+                    `
+                )}
+            >
+                {items.map((item, index) => (
+                    <div
+                        key={index}
+                        className={cx(
+                            'ProgressTimeline__ProgressBarItem',
+                            css`
+                                    position: absolute;
+                                    height: 100%;
+                                    &:not(:first-child) {
+                                        border-left: 1px solid #fff;
+                                    }
+                                `
+                        )}
+                        style={{
+                            width: item.sizePercentage + '%',
+                            left: item.offsetPercentage + '%'
+                        }}
+                    >
+
+                    </div>
+                ))}
+            </div>
+            <div
+                className={cx(
+                    'ProgressTimeline__Legend',
+                    css`
+                    position: relative;
+                    width: 100%;
+                    height: 10px;
+                    margin-top: 8px;
+                `
+                )}
+            >
+                <LegendItem
+                    offset={0}
+                >
+                    {min}
+                </LegendItem>
+                {items.map((item, index) => (
+                    <LegendItem
+                        key={index}
+                        offset={item.offsetPercentage + item.sizePercentage}
+                    >
+                        {item.step}
+                    </LegendItem>
+                ))}
+            </div>
+            {indicatorEnabled ? (
+                <div
+                    className={cx(
+                        'ProgressTimeline__Indicator',
+                        css`
+                        position: absolute;
+                        top: -5px;
+                        width: 20px;
+                        height: 20px;
+                        background: #07f;
+                        border: 2px solid #fff;
+                        border-radius: 50%;
+                        margin-left: -10px;
+                    `
+                    )}
+                    style={{
+                        left: indicatorOffsetPercentage + '%'
+                    }}
+                />
+            ) : null}
+        </div>
+    )
 }
+
+export default ProgressTimeline
